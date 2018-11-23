ce5382f460d86877d569aa17366a2fb757da665d
siguiqiang <siguiqiang@hujiang.com>
2018-11-23 18:16:22 +0800

    Initial commit

---
+++ src/main/java/com/example/demo1/Controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo1.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+
+@RestController
+public class Controller {
+        @RequestMapping(value = "/hello")
+        public String hello(){
+            return  "hello Wroldhdfgdgd" ;
+        }
+
+}
